0c40a6b71f916f3d966b8f1187b84a679fb3e467
shehan-jayasinghe <jayasingheshehan1995@gmail.com>
2024-06-23 13:16:02 +0530

    implementing search functionality

--- src/app/test-parent/test-child/test-child.component.ts
+++ src/app/test-parent/test-child/test-child.component.ts
@@ -1,16 +1,29 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-test-child',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    ReactiveFormsModule,
+    FormsModule
+  ],
   templateUrl: './test-child.component.html',
   styleUrl: './test-child.component.css'
 })
 export class TestChildComponent {
   @Input() childMessage: string | undefined;
+  @Input() childItems: string[] | undefined
   @Output() messageEvent = new EventEmitter<string>();
+  @Output() childInputMessageEvent = new EventEmitter<string>();
+  childInput: string="";
   sendMessage() {
     this.messageEvent.emit('Hello from the Child Component!');
+  }
+
+  search() {
+    this.childInputMessageEvent.emit(this.childInput)
   }
 }
--- src/app/test-parent/test-parent.component.ts
+++ src/app/test-parent/test-parent.component.ts
@@ -16,8 +16,13 @@ export class TestParentComponent {
   @Input() childProperty: string | undefined;
   parentMessage: string ="";
   message: string="";
-
+  parentItems:string[] = ["Item 1", "Item 2", "Item 3"];
+  filteredItems: string[] = this.parentItems;
   receiveMessage(message: string) {
     this.message = message;
+  }
+
+  search($event: string) {
+    this.filteredItems = this.parentItems.filter(item => item === $event);
   }
 }
